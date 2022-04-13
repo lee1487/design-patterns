599472b99fb20c59f88ebd53123dc05dd325c75a
이현세 <dlgustp1487@naver.com>
2022-04-13 13:34:35 +0900

    플라이웨이트 패턴 2부 - 패턴 적용하기

---
+++ src/main/java/com/patterns/structuralpatterns/flyweight/after/Character.java
@@ -0,0 +1,15 @@
+package com.patterns.structuralpatterns.flyweight.after;
+
+public class Character {
+
+	private char value;
+	private String color;
+	private Font font;
+	public Character(char value, String color, Font font) {
+		this.value = value;
+		this.color = color;
+		this.font = font;
+	}
+	
+	
+}
---
+++ src/main/java/com/patterns/structuralpatterns/flyweight/after/Font.java
@@ -0,0 +1,24 @@
+package com.patterns.structuralpatterns.flyweight.after;
+
+public final class Font {
+
+	final String family;
+	final int size;
+
+	public Font(String family, int size) {
+		this.family = family;
+		this.size = size;
+	}
+
+	public String getFamily() {
+		return family;
+	}
+
+	public int getSize() {
+		return size;
+	}
+	
+	
+	
+	
+}
