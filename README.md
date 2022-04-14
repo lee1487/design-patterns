dd366189a665dbf57e937afaf0108ec1902f260d
이현세 <dlgustp1487@naver.com>
2022-04-14 15:31:59 +0900

    이터레이터 패턴 1부 - 패턴 소개

--- README.md
+++ README.md
@@ -313,3 +313,11 @@ https://codingdog.tistory.com/entry/java-synchronized-%EB%9D%BD%EC%9D%B4-%EC%96%
   - 단점 
     - 복잡한 문법을 표현하려면 Expression과 Parser가 복잡해진다.
 ```
+
+### 이터레이터 패턴 
+```
+이터레이터(Iterator) 패턴
+  - 집합 객체 내부 구조를 노출시키지 않고 순회 하는 방법을 제공하는 패턴 
+    - 집합 객체를 순회하는 클라이언트 코드를 변경하지 않고 다양한 순회 방법을 
+	  제공할 수 있다. 
+```
---
+++ src/main/java/com/patterns/behavioralpatterns/iterator/before/Post.java
@@ -0,0 +1,34 @@
+package com.patterns.behavioralpatterns.iterator.before;
+
+import java.time.LocalDateTime;
+
+public class Post {
+
+	private String title;
+	private LocalDateTime createdDateTime;
+
+	public Post(String title) {
+		this.title = title;
+		this.createdDateTime = LocalDateTime.now();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getCreatedDateTime() {
+		return createdDateTime;
+	}
+
+	public void setCreatedDateTime(LocalDateTime createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+	
+	
+	
+	
+}
