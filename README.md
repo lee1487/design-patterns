53c82cf2ebcbf5cae459aa467ba5da9ed907048f
이현세 <dlgustp1487@naver.com>
2022-04-14 11:27:54 +0900

    커맨드 패턴 1부 - 패턴 소개

--- README.md
+++ README.md
@@ -283,3 +283,10 @@ https://codingdog.tistory.com/entry/java-synchronized-%EB%9D%BD%EC%9D%B4-%EC%96%
   - 단점 
     - 디버깅이 조금 어렵다. 
 ```
+
+### 커맨드 패턴 
+```
+커맨드(Command) 패턴 
+  - 요청을 캡슐화 하여 호출자(invoker)와 수신자(receiver)를 분리하는 패턴 
+    - 요청을 처리하는 방법이 바뀌더라도, 호출자의 코드는 변경되지 않는다. 
+```
---
+++ src/main/java/com/patterns/behavioralpatterns/command/before/Button.java
@@ -0,0 +1,24 @@
+package com.patterns.behavioralpatterns.command.before;
+
+public class Button {
+
+	private Light light;
+
+	public Button(Light light) {
+		this.light = light;
+	}
+	
+	public void press() {
+		light.off();
+	}
+	
+	public static void main(String[] args) {
+		Button button = new Button(new Light());
+		button.press();
+		button.press();
+		button.press();
+		button.press();
+	}
+	
+	
+}
