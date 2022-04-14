3fc6eb8bd395b5cdc12e465455f0e936c62304ef
이현세 <dlgustp1487@naver.com>
2022-04-14 14:58:46 +0900

    인터프리터 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/com/patterns/behavioralpatterns/interpreter/java/App.java
@@ -0,0 +1,15 @@
+package com.patterns.behavioralpatterns.interpreter.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
+	}
+}
