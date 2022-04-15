9cbbe9e50216b52ef0aed8936ae4c86287a4e253
이현세 <dlgustp1487@naver.com>
2022-04-15 14:40:58 +0900

    옵저버 패턴 5부 - 스프링에서 찾아보는 패턴

---
+++ src/main/java/com/patterns/behavioralpatterns/observer/java/MyRunner.java
@@ -0,0 +1,22 @@
+package com.patterns.behavioralpatterns.observer.java;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+	
+	private ApplicationEventPublisher publisher;
+	
+	public MyRunner(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		publisher.publishEvent(new MyEvent("hello spring event"));
+	}
+
+}
